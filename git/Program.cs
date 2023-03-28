b329a147ee563fe83c1106a10d0e8d0e124bc969
Vano <vanolox@ggg.fgg>
2023-03-28 15:37:06 +0300

    test

--- git/Program.cs
+++ git/Program.cs
@@ -10,6 +10,10 @@ namespace git
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите а");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            int b = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
